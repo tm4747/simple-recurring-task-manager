30ada468df403696148aa9559cd54deef538482e
Tom Molinaro <tommolinaro@Toms-Mac-mini>
2026-08-16 00:04:44 -0400

    initial commit

---
+++ SimpleRecurringTaskManager/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SimpleRecurringTaskManager
+//
+//  Created by Tom Molinaro on 8/16/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SimpleRecurringTaskManager/SimpleRecurringTaskManagerApp.swift
@@ -0,0 +1,32 @@
+//
+//  SimpleRecurringTaskManagerApp.swift
+//  SimpleRecurringTaskManager
+//
+//  Created by Tom Molinaro on 8/16/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SimpleRecurringTaskManagerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
